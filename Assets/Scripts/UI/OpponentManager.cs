e5e72a7f643b29dc67f619c52c9c82a7240fc4df
ahmad-karim <116361254+ahmad-karim-q@users.noreply.github.com>
2025-05-12 17:46:38 +0500

    commit

---
+++ Assets/Scripts/UI/OpponentManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpponentManager : MonoBehaviour
+{
+    public GameObject[] opponentCharacters;
+
+    void Start()
+    {
+        if (opponentCharacters.Length == 0)
+        {
+            Debug.LogError("No opponents assigned to the OpponentManager!");
+            return;
+        }
+        ActivateRandomOpponent();
+    }
+
+    void ActivateRandomOpponent()
+    {
+        int randomIndex = Random.Range(0, opponentCharacters.Length);
+        for (int i = 0; i < opponentCharacters.Length; i++)
+        {
+            if (i == randomIndex)
+            {
+                opponentCharacters[i].SetActive(true);
+            }
+            else
+            {
+                opponentCharacters[i].SetActive(false);
+            }
+        }
+    }
+}
